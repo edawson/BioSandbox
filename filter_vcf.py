c3d0e498e7e0f30efcfb8a0ead713faef02aa178
Eric T. Dawson <eric.t.dawson@gmail.com>
2013-11-04 15:06:38 -0600

    Final working script. Should be tested further

--- filter_vcf.py
+++ filter_vcf.py
@@ -14,15 +14,14 @@ def load_file(infile):
             else:
                 try:
                     position = line.split("\t")[1]
-                    snp[position] = line
+                    snp[str(position)] = str(line)
                 except:
-                    print "I don't know what to do with weird VCF formatting!"
+                    print "I don't know what to do with non-standard VCF formatting!"
 
 
 def indel_filter(snp_file, indel_file):
     global snp
     global header
-    indel = {}
 
     with open(indel_file, "r") as indel_vcf:
         for line in indel_vcf:
@@ -31,26 +30,35 @@ def indel_filter(snp_file, indel_file):
             else:
                 position = line.split("\t")[1]
                 pos = int(position)
-                if (position in snp.keys() or str(pos + 1) in snp.keys()
-                or str(pos + 2) in snp.keys() or str(pos + 3) in snp.keys()
-                or str(pos - 1) in snp.keys() or str(pos + 2) in snp.keys()
-                or str(pos + 3) in snp.keys()):
-                    del snp[position]
+                window = [str(pos - 2), str(pos -1), str(pos), str(pos + 1), str(pos + 2)]
+                for x in window:
+                    if (x in snp.keys()):
+                        del snp[x]
 
-def snp_filter(snp_file):
+def snp_filter():
     global snp
-    dead_keys = ()
+    dead_keys = []
     for key in snp:
-        neighbors = ()
-        for i in snp:
-            if (abs(int(key) - int(i)) <= 5 and i != key):
-                neighbors.append(i)
-        if (len(neighbors) >= 4):
-            for x in neighbors:
-                dead_keys.append(x)
-
+        neighbors = []
+        high = int(key) + 10
+        low = int(key) - 10
+        for x in range(int(key), high):
+            if (str(x) in snp.keys() and str(x) != key):
+                neighbors.append(str(x))
+        print key + " : " + str(neighbors)
+        if (len(neighbors) >= 2):
+            if (not key in dead_keys):
+                dead_keys.append(key)
+            for y in neighbors:
+                if (y not in dead_keys):
+                    dead_keys.append(y)
+    print dead_keys
     for dead in dead_keys:
         del snp[dead]
+
+def remove_modulo():
+    for key in snp:
+        snp[key] = snp[key].replace("%", "")
 
 def write_out(outfile):
     global snp
@@ -58,8 +66,12 @@ def write_out(outfile):
     with open(outfile, "w") as out_vcf:
         for line in header:
             out_vcf.write(line)
-        for key in snp:
-            outfile.write(snp[key])
+        sort_snp = []
+        for key in snp.keys():
+            sort_snp.append(int(key))
+        sort_snp.sort()
+        for key in sort_snp:
+            out_vcf.write(snp[str(key)])
         
 def main():
     snp_file = sys.argv[1]
@@ -67,6 +79,8 @@ def main():
     name1 = sys.argv[3]
     name2 = sys.argv[4]
     load_file(snp_file)
+    remove_modulo()
+    snp_filter()
     indel_filter(snp_file, indel_file)
     outfile = name1 + "." + name2 + ".vcf"
     write_out(outfile)
